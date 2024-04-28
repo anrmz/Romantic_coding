6c9877778798b02e97c60da1871bbcaeb0858a88
anrmz <anouarmezgualli17@gmail.com>
2024-04-28 01:22:20 +0100

    first commit

--- src/Components/Romantic.jsx
+++ src/Components/Romantic.jsx
@@ -1,80 +1,80 @@
 import React from 'react';
-import "./loveTruck.css";import "./envelope.css";
+import "./loveTruck.css"; import "./envelope.css";
 
 export default function Romantic() {
-    return (
-        <div>
-          <div className="envelope-container">
-            <div className="envelope">
-              <div className="envelope__tab"></div>
-              <div className="envelope__letter">
-                <p>meeting you was like listening to a song for the first time and knowing it would be my favorite.💖 i love you darling🌍</p>
-              </div>
-              <div className="envelope--base"></div>
-            </div>
+  return (
+    <div>
+      <div className="envelope-container">
+        <div className="envelope">
+          <div className="envelope__tab"></div>
+          <div className="envelope__letter">
+            <p>💖i love you israe fenkoucha dyali🌍</p>
           </div>
-    
-          <div className="truck-container">
-            <div className="container">
-              <div className="ts">
-                <div className="box"></div>
-                <div className="front">
-                  <div className="light"></div>
-                  <div className="trapezoid"></div>
-                  <div className="pipe"></div>
-                  <div className="pipe2"></div>
-                  <img className="img1" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img2" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img3" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img4" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img5" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img6" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img7" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img8" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img9" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img10" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <img className="img11" src="https://i.gifer.com/5zoP.gif" alt="" />
-                  <div className="front1"></div>
-                  <div className="front2"></div>
-                  <div className="window"></div>
-                  <div className="hood"></div>
-                </div>
-                <div className="tyre1">
-                  <div className="r1"></div>
-                  <div className="r2"></div>
-                  <div className="r3"></div>
-                  <div className="r4"></div>
-                  <div className="r5"></div>
-                  <div className="r6"></div>
-                </div>
-                <div className="tyre2">
-                  <div className="r1"></div>
-                  <div className="r2"></div>
-                  <div className="r3"></div>
-                  <div className="r4"></div>
-                  <div className="r5"></div>
-                  <div className="r6"></div>
-                </div>
-                <div className="tyre3">
-                  <div className="r1"></div>
-                  <div className="r2"></div>
-                  <div className="r3"></div>
-                  <div className="r4"></div>
-                  <div className="r5"></div>
-                  <div className="r6"></div>
-                </div>
-                <div className="tyre4">
-                  <div className="r1"></div>
-                  <div className="r2"></div>
-                  <div className="r3"></div>
-                  <div className="r4"></div>
-                  <div className="r5"></div>
-                  <div className="r6"></div>
-                </div>
-              </div>
+          <div className="envelope--base">
+          </div>
+        </div>
+      </div>
+
+      <div className="truck-container">
+        <div className="container">
+          <div className="ts">
+            <div className="box"></div>
+            <div className="front">
+              <div className="light"></div>
+              <div className="trapezoid"></div>
+              <div className="pipe"></div>
+              <div className="pipe2"></div>
+              <img className="img1" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img2" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img3" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img4" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img5" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img6" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img7" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img8" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img9" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img10" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <img className="img11" src="https://i.gifer.com/5zoP.gif" alt="" />
+              <div className="front1"></div>
+              <div className="front2"></div>
+              <div className="window"></div>
+              <div className="hood"></div>
+            </div>
+            <div className="tyre1">
+              <div className="r1"></div>
+              <div className="r2"></div>
+              <div className="r3"></div>
+              <div className="r4"></div>
+              <div className="r5"></div>
+              <div className="r6"></div>
+            </div>
+            <div className="tyre2">
+              <div className="r1"></div>
+              <div className="r2"></div>
+              <div className="r3"></div>
+              <div className="r4"></div>
+              <div className="r5"></div>
+              <div className="r6"></div>
+            </div>
+            <div className="tyre3">
+              <div className="r1"></div>
+              <div className="r2"></div>
+              <div className="r3"></div>
+              <div className="r4"></div>
+              <div className="r5"></div>
+              <div className="r6"></div>
+            </div>
+            <div className="tyre4">
+              <div className="r1"></div>
+              <div className="r2"></div>
+              <div className="r3"></div>
+              <div className="r4"></div>
+              <div className="r5"></div>
+              <div className="r6"></div>
             </div>
           </div>
         </div>
-      );
-    }
-        
+      </div>
+    </div>
+  );
+}
